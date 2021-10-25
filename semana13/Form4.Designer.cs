b3000d755c19d51dee4898a5ad7cc51a93c88877
KEVIN RODRIGO CORCIO BONILLA <USIS017621@ugb.edu.sv>
2021-10-24 20:41:46 -0600

    agregado

--- semana13/Form4.Designer.cs
+++ semana13/Form4.Designer.cs
@@ -33,7 +33,12 @@ namespace semana13
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -59,7 +64,7 @@ namespace semana13
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(319, 170);
+            this.button1.Location = new System.Drawing.Point(319, 160);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 31);
             this.button1.TabIndex = 2;
@@ -69,12 +74,52 @@ namespace semana13
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(439, 170);
+            this.button2.Location = new System.Drawing.Point(439, 160);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(78, 31);
             this.button2.TabIndex = 3;
             this.button2.Text = "volver ";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(314, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(354, 87);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "hamburguesa de doble carne \r\nuna papa \r\nuna soda \r\n";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::semana13.Properties.Resources.hamburguesa_de_carne;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 223);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(273, 165);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(319, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 31);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "seleccionar ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(439, 342);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 31);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "volver ";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
@@ -82,6 +127,10 @@ namespace semana13
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OrangeRed;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -89,6 +138,7 @@ namespace semana13
             this.Name = "Form4";
             this.Text = "Form4";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,5 +150,9 @@ namespace semana13
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- semana13/Properties/Resources.Designer.cs
+++ semana13/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace semana13.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hamburguesa_de_carne {
+            get {
+                object obj = ResourceManager.GetObject("hamburguesa de carne", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
